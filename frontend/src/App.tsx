99c08a3a755177ed01a770f4c447b33b1d596685
CapSap <cshotam@gmail.com>
2023-12-20 00:00:59 +1100

    setup basic state to handle user input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react"
 
 function App() {
+
+  const [query, setQuery] = useState<string>()
+
+  function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>)  {
+    setQuery(e.target.value)
+  }
 
   return (
     <>
      <div className='query-input'>
       <label htmlFor="query-input">Query Input</label>
-      <textarea name="queryInput" id="query-input" cols={30} rows={10}></textarea>
+      <textarea name="queryInput" id="query-input" cols={30} rows={10} onChange={(e) => handleChange(e)}></textarea>
       </div> 
     </>
   )
